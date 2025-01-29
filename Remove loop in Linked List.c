ad2870b6c1b2dfe05c040bb06815f58a6a4ad1c4
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-30 02:26:44 +0530

    Create Remove loop in Linked List.c

---
+++ Remove loop in Linked List.c
@@ -0,0 +1,52 @@
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class Solution 
+{
+    public:
+     
+        void removeLoop(Node* head) 
+        {
+            Node *fast, *slow;
+            fast = slow = head;
+            
+            while (fast != NULL && fast -> next != NULL)
+            {
+                fast = fast -> next -> next;
+                slow = slow -> next;
+                if (fast == slow)
+                {
+                    slow = head;
+                    if (slow == fast) 
+                    {
+                        while(fast -> next != slow) 
+                        {
+                            fast = fast -> next;
+                        }
+                    }
+                    
+                    else 
+                    {
+                        while (slow -> next != fast -> next) 
+                        {
+                            slow = slow -> next;
+                            fast = fast -> next;
+                        }
+                    }
+ 
+                    fast -> next = NULL;
+                }
+            }
+        }
+};
+
+// Link to the problem: https://www.geeksforgeeks.org/problems/remove-loop-in-linked-list/1/
