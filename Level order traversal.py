8e47466a53a5255160f7e8af568cdec3e832f0fd
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-02 19:21:13 +0530

    Create Level order traversal.py

---
+++ Level order traversal.py
@@ -0,0 +1,27 @@
+"""
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+"""
+class Solution:
+    def levelOrder(self, root):
+        # Your code here
+        ans = []
+        if root is None:
+            return ans
+        q = deque([root])
+        while q:
+            t = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                t.append(node.data)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(t)
+        return ans
+
+# Link to the problem: https://www.geeksforgeeks.org/problems/level-order-traversal/1/
