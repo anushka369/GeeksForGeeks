c33b10bd269d64a7c1c27e273d46b1c861dc128b
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-12 22:15:39 +0530

    Create k-th Smallest in BST.cpp

---
+++ Medium/k-th Smallest in BST.cpp
@@ -0,0 +1,41 @@
+/*
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) 
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution 
+{
+    public:
+        void inorder(Node* root, int &k, int &ans)
+        {
+            if (!root) return ;
+            inorder(root -> left, k, ans);
+            k--;
+          
+            if (k == 0)
+            {
+                ans = root -> data;
+                return ;
+            }
+            inorder(root -> right, k, ans);
+        }
+    
+        int kthSmallest(Node *root, int k) 
+        {
+            int ans = -1;
+            inorder(root, k, ans);
+            return ans;
+        }
+};
+
+// Link to the problem: https://www.geeksforgeeks.org/problems/find-k-th-smallest-element-in-bst/1/
