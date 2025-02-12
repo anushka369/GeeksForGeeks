9985d91044fdb9b366dc3db33f557d2fe3765436
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-12 22:19:15 +0530

    Update Permutations of a String.cpp

--- Medium/Permutations of a String.cpp
+++ Medium/Permutations of a String.cpp
@@ -1,35 +1,37 @@
-class Solution {
-  public:
-    vector<string> findPermutation(string &s) {
-        vector<string> result;
-        sort(s.begin(), s.end());
-        backtrack(s, 0, result);
-        return result;
-    }
-
-  private:
-    void backtrack(string &s, int index, vector<string> &result) 
-    {
-        if (index == s.size()) 
+class Solution 
+{
+    public:
+        vector<string> findPermutation(string &s) 
         {
-            result.push_back(s); 
-            return;
+            vector<string> result;
+            sort(s.begin(), s.end());
+            backtrack(s, 0, result);
+            return result;
         }
 
-        unordered_set<char> used;
-        for (int i = index; i < s.size(); i++) 
+    private:
+        void backtrack(string &s, int index, vector<string> &result) 
         {
-            if (used.find(s[i]) != used.end()) 
+            if (index == s.size()) 
             {
-                continue; 
+                result.push_back(s); 
+                return;
             }
-
-            used.insert(s[i]);
-            swap(s[index], s[i]);       
-            backtrack(s, index + 1, result);
-            swap(s[index], s[i]);
+    
+            unordered_set<char> used;
+            for (int i = index; i < s.size(); i++) 
+            {
+                if (used.find(s[i]) != used.end()) 
+                {
+                    continue; 
+                }
+    
+                used.insert(s[i]);
+                swap(s[index], s[i]);       
+                backtrack(s, index + 1, result);
+                swap(s[index], s[i]);
+            }
         }
-    }
 };
 
 // Link to the problem: https://www.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1/
