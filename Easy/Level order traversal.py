82dd49d6758159262adccb90da7defc486b3dcf9
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-12 22:20:04 +0530

    Update Level order traversal.py

--- Easy/Level order traversal.py
+++ Easy/Level order traversal.py
@@ -5,20 +5,24 @@ class Node:
         self.data = value
         self.right = None
 """
+
 class Solution:
     def levelOrder(self, root):
-        # Your code here
         ans = []
+        
         if root is None:
             return ans
         q = deque([root])
+        
         while q:
             t = []
             for _ in range(len(q)):
                 node = q.popleft()
                 t.append(node.data)
+                
                 if node.left:
                     q.append(node.left)
+                    
                 if node.right:
                     q.append(node.right)
             ans.append(t)
