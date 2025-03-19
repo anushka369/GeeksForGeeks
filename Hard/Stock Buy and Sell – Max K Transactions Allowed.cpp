1cc21a28befe56a9ce492f3dfd81bde936f11ae3
anushka <81908360+anushka369@users.noreply.github.com>
2025-03-19 23:59:39 +0530

    Update Stock Buy and Sell – Max K Transactions Allowed.cpp

--- Hard/Stock Buy and Sell – Max K Transactions Allowed.cpp
+++ Hard/Stock Buy and Sell – Max K Transactions Allowed.cpp
@@ -4,20 +4,29 @@ using namespace std;
 class Solution 
 {
     public:
-        int solve(vector<int>& prices, int k,int i,bool canBuy,vector<vector<vector<int>>>&dp)
+        int solve(vector <int> & prices, int k, int i, bool canBuy, vector <vector <vector <int>>> &dp)
         {
-            if(i==prices.size() || k==0 ) return 0;
-            if(dp[i][k][canBuy] != -1) return dp[i][k][canBuy];
-            int involve =0, ignore=0;
-            if(canBuy==true)
+            if (i == prices.size() || k == 0) 
             {
-                involve=-prices[i] + solve(prices,k,i+1,false,dp);
-                ignore=solve(prices,k,i+1,true,dp);
+                return 0;
+            }
+            
+            if (dp[i][k][canBuy] != -1) 
+            {
+                return dp[i][k][canBuy];
+            }
+            
+            int involve = 0, ignore = 0;
+            
+            if (canBuy == true)
+            {
+                involve =- prices[i] + solve(prices, k, i + 1, false, dp);
+                ignore = solve(prices, k, i + 1, true, dp);
             }
               
             else
             {
-                involve=prices[i] + solve(prices,k-1,i+1,true,dp);
+                involve = prices[i] + solve(prices, k - 1, i + 1, true, dp);
                 ignore=solve(prices,k,i+1,false,dp);
             }
           
