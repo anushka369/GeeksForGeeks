5b1a71ba7da4753be84de4a83df9635c7573d6fe
anushka <81908360+anushka369@users.noreply.github.com>
2025-03-20 00:01:49 +0530

    Create Stock Buy and Sell – Max K Transactions Allowed.cpp

--- Hard/Stock Buy and Sell – Max K Transactions Allowed.cpp
+++ Hard/Stock Buy and Sell – Max K Transactions Allowed.cpp
@@ -27,31 +27,32 @@ class Solution
             else
             {
                 involve = prices[i] + solve(prices, k - 1, i + 1, true, dp);
-                ignore=solve(prices,k,i+1,false,dp);
+                ignore=solve(prices, k, i + 1, false, dp);
             }
           
-            return dp[i][k][canBuy]= max(involve,ignore);
+            return dp[i][k][canBuy] = max(involve, ignore);
         }
 
-        int maxProfit(vector<int>& prices, int k) 
-        {
-            int n=prices.size();
-            vector<vector<vector<int>>>dp(n,vector<vector<int>>(k+1,vector<int>(2,-1)));
-            return solve (prices, k, 0, true, dp);
-            
+        int maxProfit(vector <int> & prices, int k) 
+        { 
+            int n = prices.size();
+            vector <vector <vector <int>>> dp(n, vector <vector <int>> (k + 1, vector <int> (2, -1)));
+            return solve (prices, k, 0, true, dp);            
         }
 };
 
-//{ Driver Code Starts.
-
-int main() {
+// Driver Code
+int main() 
+{
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
     int t;
     cin >> t;
     cin.ignore();
-    while (t--) {
+    
+    while (t--) 
+    {
         string input;
         getline(cin, input);
         istringstream iss(input);
@@ -59,9 +60,11 @@ int main() {
         int num;
 
         // Read integers from the input string
-        while (iss >> num) {
+        while (iss >> num) 
+        {
             arr.push_back(num);
         }
+        
         int k;
         cin >> k;
         cin.ignore();
@@ -69,6 +72,8 @@ int main() {
         cout << ob.maxProfit(arr, k) << endl;
         cout << "~" << endl;
     }
+    
     return 0;
 }
-// } Driver Code Ends
+
+// Link to the problem: https://www.geeksforgeeks.org/problems/maximum-profit/1/
